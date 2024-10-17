9a3a5cad4d9f836a3943b1f26caa074e39f77515
cocofrate <74274316+cocofrate@users.noreply.github.com>
2024-10-17 22:18:51 +0200

    Ajustement

--- attackairship/client.lua
+++ attackairship/client.lua
@@ -1,51 +1,58 @@
 ----------------------config-------------------------
-
-rayon = 50 -- Set bombing radius here  
-
-speedbomb = 30 -- Time between each falling bomb (in mlsc)
-
-strikeCount = 20 -- Number of bombs dropped when the button is pressed
-
+local BOMB_RADIUS = 70 -- Set bombing radius here
+local BOMB_INTERVAL = 30 -- Time between each falling bomb (in ms)
+local BOMB_COUNT = 20 -- Number of bombs dropped when the button is pressed
+local AIRSHIP_HASH = 0x7F701CD2 -- Hash of the airship model
+local WEAPON_HASH = GetHashKey("WEAPON_AIRSTRIKE_ROCKET") -- Hash of the weapon used
 -----------------------------------------------------
 
 Citizen.CreateThread(function()
-  RequestWeaponAsset(GetHashKey("WEAPON_AIRSTRIKE_ROCKET")) 
-  while not HasWeaponAssetLoaded(GetHashKey("WEAPON_AIRSTRIKE_ROCKET")) do
-      Wait(0)
-  end
-  while true do
-    Citizen.Wait(500)
-    ped = PlayerPedId()
-    hashAirship = 0x7F701CD2
-    if IsPedInModel(ped, hashAirship) then
-      AddTextEntry("BOMB", "Dirigeable du FLNC :\nAppuie sur ~INPUT_VEH_DROP_PROJECTILE~ pour bombarder.\n")
-      DisplayHelpTextThisFrame("BOMB", false)
-      break
+    RequestWeaponAsset(WEAPON_HASH)
+    while not HasWeaponAssetLoaded(WEAPON_HASH) do
+        Citizen.Wait(0)
     end
-  end
-
+    
+    while true do
+        Citizen.Wait(500)
+        local playerPed = PlayerPedId()
+        if IsPedInModel(playerPed, AIRSHIP_HASH) then
+            AddTextEntry("BOMB", "FLNC Airship:\nAppuyez sur ~INPUT_VEH_DROP_PROJECTILE~ pour bombarder.\n")
+            DisplayHelpTextThisFrame("BOMB", false)
+            break
+        end
+    end
 end)
 
 Citizen.CreateThread(function()
-  hashAirship = 0x7F701CD2
-  while true do
-    if IsPedInModel(ped, hashAirship) then
-      coords = GetEntityCoords(ped)
-      bombardement("WEAPON_AIRSTRIKE_ROCKET")
+    while true do
+        Citizen.Wait(1)
+        local playerPed = PlayerPedId()
+        if IsPedInModel(playerPed, AIRSHIP_HASH) then
+            local playerCoords = GetEntityCoords(playerPed)
+            Bombardment(WEAPON_HASH, playerCoords)
+        end
     end
-    Citizen.Wait(1)
-  end
 end)
 
-function bombardement(arme)
-  if IsControlPressed(0, 105) then
-    o = 0
-    while o < strikeCount do
-      randomX = math.random(-rayon, rayon)
-      randomY = math.random(-rayon, rayon)
-      ShootSingleBulletBetweenCoords(coords.x+randomX, coords.y+randomY, coords.z-10, coords.x+randomX, coords.y+randomY, coords.z-500, 500, true,GetHashKey(arme), ped, true, false, 0)
-      o = o + 1
-      Citizen.Wait(speedbomb)
+function Bombardment(weapon, coords)
+    if IsControlPressed(0, 105) then
+        for bombIndex = 1, BOMB_COUNT do
+            local randomX = math.random(-BOMB_RADIUS, BOMB_RADIUS)
+            local randomY = math.random(-BOMB_RADIUS, BOMB_RADIUS)
+            local bombStartPos = vector3(coords.x + randomX, coords.y + randomY, coords.z - 10)
+            local bombEndPos = vector3(coords.x + randomX, coords.y + randomY, coords.z - 500)
+            
+            -- Visual explosion effect
+            AddExplosion(bombEndPos.x, bombEndPos.y, bombEndPos.z, 29, 1.0, true, false, 1.0)
+            
+            -- Fire the bomb
+            ShootSingleBulletBetweenCoords(
+                bombStartPos.x, bombStartPos.y, bombStartPos.z,
+                bombEndPos.x, bombEndPos.y, bombEndPos.z,
+                500, true, weapon, PlayerPedId(), true, false, 0
+            )
+            
+            Citizen.Wait(BOMB_INTERVAL)
+        end
     end
-  end
 end
